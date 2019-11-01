d7f74416dc2c262c6886cf3500a034d4b0613a73
Ronak0020 <49647021+Ronak0020@users.noreply.github.com>
2019-11-01 15:40:12 +0530

    Update urban.js

--- commands/Urban Dictionary/urban.js
+++ commands/Urban Dictionary/urban.js
@@ -8,7 +8,7 @@ exports.urban = {
 			usage: "<word>",
 			description: "looks up a word on Urban Dictionary",
 			process: function(bot,msg,suffix){
-					var targetWord = suffix == "" : urban(suffix);
+					var targetWord = suffix == "" ? urban(suffix);
 					targetWord.first(function(json) {
 							if (json) {
 								var message = "Urban Dictionary: **" +json.word + "**\n\n" + json.definition;
